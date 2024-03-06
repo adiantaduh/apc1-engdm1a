77b927dd88ac4a5bfa50944cd086a95ad055c0a4
adiantaduh <85759357+adiantaduh@users.noreply.github.com>
2024-03-06 13:52:37 +0000

    codigos da aula2 e da pratica1

---
+++ aulas/aula02/tipos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // 'a','1', '\n'
+  int inteiro; // 0, 1, 2, 3, -1, -2, -3
+  float decimal6casas; // -15.000000, 15.0000
+  double decimal15casas; // -15e+15, 15e+15
+  void sem_valor; 
+  // string nao existe na linguagem C, "uma frase qualquer"
+  
+  return 0;
+}
